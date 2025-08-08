e916f962cd2c6660c39605c279d44d91a0245402
Juliana <julianafuchsnyc@gmail.com>
2025-08-08 09:52:25 -0400

    Initializer and class created

--- Birthdays/BirthdaysApp.swift
+++ Birthdays/BirthdaysApp.swift
@@ -1,10 +1,3 @@
-//
-//  BirthdaysApp.swift
-//  Birthdays
-//
-//  Created by Scholar on 8/8/25.
-//
-
 import SwiftUI
 
 @main
--- Birthdays/ContentView.swift
+++ Birthdays/ContentView.swift
@@ -1,19 +1,15 @@
-//
-//  ContentView.swift
-//  Birthdays
-//
-//  Created by Scholar on 8/8/25.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+//    @State private var friends: [Friend] = [
+//        Friend(name: "Julia", birthday: .now) ,
+//        Friend(name: "Sammi", birthday: Date(timeIntervalSince1970: <#T##TimeInterval#>))
+//    ]
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            
+            
         }
         .padding()
     }
